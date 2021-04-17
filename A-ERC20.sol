cca29438cecc96bc0d793431b541353ae286e9ab
ZeroR0R <59980861+ZeroR0R@users.noreply.github.com>
2021-04-17 17:53:19 -0400

    Updated
    Added a new feature VIPs. VIPs are now need to hire workers to create new tokens. Changed a few other functions and made it so workers have a limited amount rewards they can mint. Instead of having the VIPs in the same file I decided to make a new file for organization purposes, easy editing, and better security. I plan to move the workers to a new file as well making this file exclusively implementation.

--- A-ERC20.sol
+++ A-ERC20.sol
@@ -1,17 +1,19 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "./vips.sol";
 
-contract Return_R0R is ERC20 {
+contract Return_R0R is ERC20, VIPs {
     
     uint256 initialSupply = 1000;
-    uint256 Reward = 10;
+    uint256 Reward = 1;
+    uint256 MidReward = 20;
     address owner;
     
     struct Worker {
         address addr;
-        uint256 number;
         string name;
+        uint256 mints;
         bool valid;
     }
     
@@ -20,6 +22,13 @@ contract Return_R0R is ERC20 {
     modifier onlyWorker() {
         
         require(worker[msg.sender].valid == true, "Only workers can call this function");
+        _;
+        
+    }
+    
+    modifier onlyVIP() {
+        
+        require(isVIP[msg.sender] == true, "Only VIPs can call this function");
         _;
         
     }
@@ -35,15 +44,45 @@ contract Return_R0R is ERC20 {
         
     }
     
-    function makeWorker(address _worker, uint256 _number, string memory _name) public onlyOwner {
+    function hireWorker(address _worker, string memory _name) public onlyVIP {
         
-        worker[_worker] = Worker(_worker, _number, _name, true);
+        worker[_worker] = Worker(_worker, _name, 0, true);
         
     }
     
     function mintReward() public onlyWorker {
+        require(worker[msg.sender].mints <= 5, "You have already minted the maximum amount of times");
         _mint(msg.sender, Reward);
+        worker[msg.sender].mints = worker[msg.sender].mints + 1;
         
+    }
+    
+    function mintMediumReward() public onlyVIP {
+        
+        require(hasMintedMid[msg.sender] == false, "You have already minted a reward.");
+        require(WorkersHired[msg.sender] > 4, "You need to hire five more workers in order to mint a large reward");
+        _mint(msg.sender, MidReward);
+        hasMintedMid[msg.sender] = true;
+        
+    }
+    
+    function buyVIPLicense() public onlyWorker {
+        
+        require(isVIP[msg.sender] == false, "You have already bought this!");
+        require(balanceOf(msg.sender) >= VIP_License_Price);
+        transfer(owner, VIP_License_Price);
+        isVIP[msg.sender] = true;
+        WorkersHired[msg.sender] = 0;
+        hasMintedMid[msg.sender] = false;
+        
+    }
+    
+    function setVIP(address _user) public onlyOwner {
+        
+        require(isVIP[_user] == false, "This user already has this item");
+        isVIP[_user] = true;
+        WorkersHired[_user] = 0;
+        hasMintedMid[_user] = false;
     }
     
 }
